bc527060fea99e597ff33784b80325e501c3d981
JuanAmbordt <juaniambordt@gmail.com>
2024-06-06 16:51:25 -0300

    Clase clsProcesosBD 06062024

---
+++ clsProcesosBD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCosmetica
+{
+    internal class clsProcesosBD
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string varCadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Cosmetica.accdb";
+    }
+}
