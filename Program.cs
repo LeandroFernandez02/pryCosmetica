71c821b31f961afd32611bdee2d19ce1bb3c2612
Juan <juancruzcalvetw@gmail.com>
2024-06-18 01:01:16 -0300

    Cambios varios

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace pryCosmetica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmBuscarEmpleado());
+            Application.Run(new frmInicioSesion());
         }
     }
 }
